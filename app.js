6ff9122d68f525b1b8c6aa5cbedd2f52cef952f3
panecatl <ypanecatl24@gmail.com>
2022-06-23 14:53:14 -0500

    added html funct

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
+// importing the tool to create the html file
+const fs = require('fs');
+// importing the page-template.js from src folder
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-  // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-    }
+const [name, github] = profileDataArgs;
 
-    console.log('================');
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-  // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Checkout index.html to see the output!')
+});
