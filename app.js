bfce9685e15bda036396ff4f4e9912700d5c2ce9
panecatl <ypanecatl24@gmail.com>
2022-06-26 17:51:30 -0500

    inquirer is working

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-// importing the tool to create the html file
-const fs = require('fs');
-// importing the page-template.js from src folder
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+inquirer 
+  .prompt([{
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  }
+])
+.then(answers => console.log(answers)); 
 
-const profileDataArgs = process.argv.slice(2);
 
-const [name, github] = profileDataArgs;
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// // importing the tool to create the html file
+// const fs = require('fs');
+// // importing the page-template.js from src folder
+// const generatePage = require('./src/page-template.js');
 
-  console.log('Portfolio complete! Checkout index.html to see the output!')
-});
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw (err);
+
+//   console.log('Portfolio complete! Checkout index.html to see the output!')
+// });
